d9c9159516ba6f55cff8d8daaa73faf2f9265e21
robertoyubero <robertoyubero@gmail.com>
2015-09-28 10:56:22 +0200

    calculadora

--- calc.py
+++ calc.py
@@ -3,8 +3,43 @@
 
 # Vamos a programar una calculadora
 
+import sys                          #importamos el paquete sys para leer del teclado!
+
 def main():
-    pass
+    pass;
+
+#definimos la suma
+def suma(sumando1, sumando2):
+    return sumando1 + sumando2
+    
+#definimos la division
+def division(dividendo, divisor):
+    return dividendo / divisor
+    
+#definimos la division
+
 
 if __name__ == "__main__":
-    main()
+
+    try:
+        print("\n1) OPERACION SUMA:\n"+ "Introduce el sumando a = ", end = "")
+        operando1 = int(input())
+        print("Introduce el sumando b = ", end = "")
+        operando2 = int(input())
+
+    except ValueError:
+        sys.exit("¡Tienen que ser enteros!")
+    print("El resultado de la suma es = " + str(suma(operando1, operando2)))
+
+
+    try:
+        print("\n2) OPERACION DIVISION:\n"+ "Introduce el dividendo D = ", end = "")
+        dividendo = float(input())
+        print("Introduce el divisor d = ", end = "")
+        divisor = float(input())        
+        
+    except ZeroDivisionError: 
+        sys.exit("No puedes dividir por 0")    
+    print("El resultado de la division es = " + str(division(dividendo,divisor)))
+    
+    
